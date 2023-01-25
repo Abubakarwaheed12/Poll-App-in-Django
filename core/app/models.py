a1715c90d36e8f73639df7dacbf00896f20f45f4
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-01-25 09:27:18 +0500

    ..

--- core/app/models.py
+++ core/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
@@ -18,3 +19,7 @@ class Choice(models.Model):
     
     
     # add new model of voters
+    class Voter(models.Model):
+        user = models.ForeignKey(User , on_delete=models.CASCADE)
+        poll = models.ForeignKey(Question , on_delete=models.CASCADE)
+        
